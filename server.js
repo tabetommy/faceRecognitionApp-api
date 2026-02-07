742bae40c17750c78cb1c10f95369b79b82964a1
tabetommy <tabe_e@yahoo.ca>
2026-02-07 10:23:30 +0100

    added prod site  to cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const rateLimit = require('express-rate-limit');
 
 const cors= require("cors");
 const corsOptions ={
-    origin:'http://localhost:3001', 
+    origin:['http://localhost:3000','https://tabetommy.github.io/facerecognition'], 
     methods: ['GET', 'POST', 'PUT'],
     credentials:true,           
     optionSuccessStatus:200,
